df80f0ef33069e3a524b3ccd85a25d6ef5766c94
Greatman Blessed <blessedgreatman96@gmail.com>
2025-01-27 14:57:11 +0100

    `Update font size of EL-KANEMI club text in index.blade.php`

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
             <div class="club-area">
                 <div class="club-logo"><img src="assets/images/items/clubs.png" alt=""></div>
                 <div class="content">
-                    <h3 class="club-text" style="font-size: 14px !important;">EL-KANEMI<BR>
+                    <h3 class="club-text" style="font-size: 60px !important;">EL-KANEMI<BR>
                         RANGERS INTL</H3>
                 </DIV>
                 <span class="match-type">NFPL MATCH</span>
